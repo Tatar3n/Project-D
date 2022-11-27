a183f51a976a583be7cd449d66e07bce64ddeba9
Tatar3n <ivanchub2104@gmail.com>
2022-11-27 12:47:44 +0300

    Add PlayerMovement Script

---
+++ Tem/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+
+    private void Start() {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate() {
+        moveDelta = Vector3.zero;
+
+        float x = Input.GetAxisRaw("Horizontal");
+
+        Debug.Log(x);
+
+        moveDelta = new Vector3(x,0,0);
+
+        if(moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1,1,1);
+
+        transform.Translate(moveDelta * Time.deltaTime);
+    }
+
+}
