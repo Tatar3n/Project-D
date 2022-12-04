ba704fde63add3ba193154b00ef419378e81a5f1
Tatar3n <ivanchub2104@gmail.com>
2022-12-04 17:52:23 +0300

    Add Zero-Version Functional

---
+++ Tem/Assets/Scripts/DamageableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageableObject : MonoBehaviour
+{
+    [SerializeField] protected float hp;
+
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Tem/Assets/Scripts/EnemyHP.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : DamageableObject
+{
+
+    private void Update()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
